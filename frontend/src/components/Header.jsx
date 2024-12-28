ae655ccaf5e5fdb62f81827b82f8fab33f8e5ad0
viqarrr <viqarmulya@gmail.com>
2024-12-28 12:35:54 +0700

    add logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,35 +4,55 @@ import { createPortal } from "react-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addOpen } from "../slices/modalSlice";
 import CreateEditModal from "../modals/CreateEditModal";
+import logo from "/logo.svg";
 
 const Header = () => {
   const add = useSelector((state) => state.modal.add);
   const dispatch = useDispatch();
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const navbarRef = useRef(null);
   const toggleNavbar = () => setIsOpen(!isOpen);
-  const handleClickOutside= (event) => {
-    if(navbarRef.current && !navbarRef.current.contains(event.target)){
-      setIsOpen(false)
-      console.log(isOpen)
+  const handleClickOutside = (event) => {
+    if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+      setIsOpen(false);
+      console.log(isOpen);
     }
-  }
+  };
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <div className={`bg-white dark:bg-gray-900 w-full fixed z-20 top-0 start-0 ${isOpen ? '' : 'border-b border-gray-200 dark:border-gray-600'}`} >
+    <div
+      className={`bg-white dark:bg-gray-900 w-full fixed z-20 top-0 start-0 ${
+        isOpen ? "" : "border-b border-gray-200 dark:border-gray-600"
+      }`}
+    >
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
-          to={'/'}
-          className="flex items-center space-x-3 rtl:space-x-reverse"
+          to={"/"}
+          className="flex items-center justify-center space-x-2 rtl:space-x-reverse"
         >
-          <span className="self-center text-xl sm:text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <svg
+            className="w-8 h-8 text-gray-800 dark:text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6 2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 1 0 0-2h-2v-2h2a1 1 0 0 0 1-1V4a2 2 0 0 0-2-2h-8v16h5v2H7a1 1 0 1 1 0-2h1V2H6Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span className="self-center text-xl sm:text-2xl leading-8 font-semibold whitespace-nowrap dark:text-white">
             ScrollShelf
           </span>
         </Link>
@@ -69,40 +89,42 @@ const Header = () => {
           </button>
         </div>
         <div
-          className={`${isOpen ? 'block' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`}
+          className={`${
+            isOpen ? "block" : "hidden"
+          } items-center justify-between w-full md:flex md:w-auto md:order-1`}
           id="navbar-sticky"
         >
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
-                to={'/education'}
+                to={"/education"}
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
+              >
                 Education
               </Link>
             </li>
             <li>
               <Link
-                to={'/sport'}
+                to={"/sport"}
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
+              >
                 Sport
               </Link>
             </li>
             <li>
               <Link
-                to={'/technology'}
+                to={"/technology"}
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
+              >
                 Technology
               </Link>
             </li>
             <li>
               <Link
                 href="#"
-                to={'/science'}
+                to={"/science"}
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Science
