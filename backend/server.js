cebe42e5cdb09ad3d59eaf8d91291acd08744483
viqarrr <viqarmulya@gmail.com>
2025-08-01 13:15:29 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT;
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-  origin: 'https://scrollshelf.netlify.app',
+  origin: process.ENV.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
